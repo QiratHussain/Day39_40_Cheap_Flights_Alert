9a32dffa1ae09c048574e012854a92a33f166508
qirat hussain <engineering4qirat@gmail.com>
2026-08-23 20:52:31 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# TODO: 1. create user sign up form
+# TODO: 2. create users data sheet
+# TODO: 3. connect users sheet with sheety
+# TODO: 4. fetch users emails from sheety
+# TODO: 5. set up emailing system
+# TODO: 6. create places_to_go sheet
+# TODO: 7. connect places sheet with sheety
+# TODO: 8. fetch places list from sheety
+# TODO: 9. find iata codes for places from sheety
+# TODO: 10. update iata codes column in places sheet
+# TODO: 11. fetch desired prices for places from sheety
+# TODO: 12. fetch iata codes from sheety in a list 
+# TODO: 12. search for flights for all iata codes
+# TODO: 13. parse json and return flight date, destination and price
+# TODO: 14. compare the flight price with desired price
+# TODO: 15. make email message if price is low
