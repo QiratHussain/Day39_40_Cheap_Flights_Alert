200dce56f9166510583a21cc18485a6c3863c052
qirat hussain <engineering4qirat@gmail.com>
2026-08-25 10:08:47 +0500

    search for flights at desired time

---
+++ flightsearch.py
@@ -0,0 +1,23 @@
+import os
+import serpapi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class FlightSearcher:
+    def __init__(self):
+        self.serpapi= os.getenv("SERP_API")
+
+    def search_flights(self, iata, start_date, end_date):
+        self.params={"engine":"google_flights", "departure_id":"LHE", "arrival_id":iata, "outbound_date": start_date, "return_date":end_date,"currency":"USD"}
+        client= serpapi.Client(api_key=self.serpapi)
+        serp_response= client.search(self.params)
+        if 'best_flights' in serp_response:
+            flights_response= serp_response['best_flights']
+            flight_price=flights_response[0]['price']
+            return flight_price
+        elif 'other_flights' in serp_response:
+            flights_response= serp_response['other_flights']
+            flight_price= flights_response[0]['price']
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from sheetydata import SheetyData
 from flightdata import IataFinder
+from flightsearch import FlightSearcher
+from datetime import datetime
+from datetime import timedelta
 
-# TODO: 9. find iata codes for places from sheety
-# TODO: 10. update iata codes column in places sheet
-# TODO: 11. fetch desired prices for places from sheety
-# TODO: 12. fetch iata codes from sheety in a list 
 # TODO: 12. search for flights for all iata codes
 # TODO: 13. parse json and return flight date, destination and price
 # TODO: 14. compare the flight price with desired price
 # TODO: 15. make email message if price is low 
 
-sheety= SheetyData()
-airport= IataFinder()
 
+# get desired prices
+sheety= SheetyData()
+desired_prices= sheety.get_desired_prices()
+
+# get iata_codes for desired places
+airport= IataFinder()
 cities_list= sheety.get_places_list()
 iata_codes_list=[]
 for city in cities_list:
@@ -21,4 +24,13 @@ for city in cities_list:
 for row in range(len(cities_list)):
     sheety.update_iata(iata_codes_list[row], row+2)
 
+# search for flights for all iata codes
+one_week_later= datetime.now()+ timedelta(days= 7)
+two_week_later= datetime.now()+ timedelta(days= 14)
+outbound_date= str(one_week_later).split(" ")
+return_date= str(two_week_later).split(" ")
+flight_searcher= FlightSearcher()
+for code in iata_codes_list:
+    flight_searched= flight_searcher.search_flights(code,outbound_date, return_date)
+    print(flight_searched)
 
